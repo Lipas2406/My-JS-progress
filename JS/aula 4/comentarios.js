db1f2e9d65e623df6125fe5db54608bf58c15a56
Lipas2406 <filipe11severo06@gmail.com>
2024-05-23 21:54:48 -0300

    coments class

---
+++ JS/aula 4/comentarios.js
@@ -0,0 +1,40 @@
+// E escrever um comentário
+console.log ("Olá Mundo!"); // Aqui tem um comentário
+console.log ("Linha 1");
+// Eu comentoonde eu quiser, e...
+console.log ("Linha 2");
+
+/* 
+* Eu sei que este será 
+  completamente ignorado,
+    não
+        importa
+            o
+                que 
+                    eu
+                        faça.
+    * Só deixará de ser um comentário contanto que 
+      haja, no final do texto, isto:
+                                    */ //Certo?
+
+console.log ("Linha 3"); // Vai ser ignorado
+console.log ("Linha 4");
+console.log ("Linha 5");
+// Simplesmente, ignorado
+
+
+// /* Quando não quero executar algum código: */ 
+
+console.log ("."); 
+console.log ("."); 
+console.log ("."); 
+console.log ("Olá Mundo!"); 
+// console.log ("easy");
+console.log ("Linha 2");
+
+/*
+console.log ("Linha 3");
+console.log ("Linha 4");
+*/
+
+console.log ("Linha 5");
