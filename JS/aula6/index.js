4cf1f0fe37937032f47d77359fa5779d4909c552
Lipas2406 <filipe11severo06@gmail.com>
2024-05-27 22:41:26 -0300

    Let class

---
+++ JS/aula6/index.js
@@ -0,0 +1,62 @@
+/*
+let nome = 'Alfredo'; // *String
+
+console.log ('Lipas tem um gato chamado', nome);
+console.log ('Lipas e', nome, 'se divertem muito juntos');
+console.log (nome, 'pegou uma doença crônica sem cura');
+console.log (nome, 'foi sacrificado por conta de seu constante sofrimento');
+console.log ('Lipas se sente sozinho sem', nome);
+*/
+
+/*
+let nome; //Declarou a variável 
+nome = 'Qualquer valor'; // *Inicializando a variável
+console.log (nome);
+nome = "Lipas";
+console.log (nome);
+*/
+
+/*
+// *Não podemos criar variáveis com palavras reservadas
+// *Variáveis precisam ter nomes significativos
+let nomeCliente = 'Lipas';
+console.log (nomeCliente);
+*/
+
+/*
+// *Não pode começar o nome de uma variável com um número
+let 1nome;
+*/
+
+/*
+// *Não podem conter espaços ou traços 
+let nome-Client;
+let nome Client;
+*/
+
+/*
+// *Utilizamos camelCase
+let nomeCompleto = 'Lipas_2406'
+console.log (nomeCompleto);
+*/
+
+/*
+// *Case-sensitive 
+let nomeCliente = 'Lipas';
+let nomecliente = 'e Alfredo';
+console.log (nomeCliente, nomecliente);
+*/
+
+/*
+// *Não podemos rededlarar variáveis com let
+let nomeCliente = 'Lipas';
+nomeCliente = 'Alfredo';
+console.log (nomeCliente);
+*/
+
+// *NÃO UTILIZE VAR, UTILIZE LET!
+
+
+
+
+console.log ('Lipas agradece');
