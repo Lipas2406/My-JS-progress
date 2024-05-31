ecf49c2ee071bf07f1b74234bfaa6370ea058b76
Lipas2406 <filipe11severo06@gmail.com>
2024-05-31 19:20:31 -0300

    var VS let class

---
+++ JS/aula9/index.js
@@ -0,0 +1,10 @@
+// ECMAScript 2015 (ES6) = JavaScript
+/*nome = 'Lipas'; //NÃO FAÇA ISSO*/
+
+
+var nome = 'Lipas';
+var nome = 'Alfredo'; //É possível redeclarar
+
+let nome = 'Lipas';
+let nome = 'Alfredo'; //Não é possível redeclarar
+console.log (nome);
