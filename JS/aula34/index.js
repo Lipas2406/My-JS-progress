b192a4a22b752d511b5bb01d9171c166859158ed
Lipas2406 <filipe11severo06@gmail.com>
2024-07-12 23:28:03 -0300

    for - classic - repetition structure class

---
+++ JS/aula34/index.js
@@ -0,0 +1,21 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+
+//       i = index
+/*
+for (let i = 400; i >= 300; i -= 3) {
+    const par = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}
+*/
+
+const frutas = ['maça', 'pera', 'uva', 'abacaxi', 'limão', 'banana', 'maracujá']
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`O índice ${i} -> `, frutas[i]);
+}
