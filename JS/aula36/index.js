4d5c1d51110c2329bfd61af86a000e4cbc22791b
Lipas2406 <filipe11severo06@gmail.com>
2024-07-16 09:51:29 -0300

    NodeList (browser) exercise class

--- JS/aula36/index.js
+++ JS/aula36/index.js
@@ -1,34 +1,28 @@
-/*DOM*/
+/* // -> Para Arrays:
+//                0        1      2        3         4         5        6        
+const frutas = ['Pera', 'Maça', 'Uva', 'Banana', 'Laranja', 'Manga', 'Limão'];
 
-// Representado por meio de uma árvore hierárquica, onde tags são pais de tags, que são pais de outras tags. Consequentemente, há diversos filhos dos filhos de tags. Há também irmãos, em que duas ou mais tags são filhos de uma mesma tag. 
-//Por exemplo:
+// for in -> lê os índices ou chaves do objeto
+for (let i in frutas) {
+    console.log(i);
+    console.log(frutas[i]);
+}
 
-/*
-<!DOCTYPE html> -> pai de todos
-<html lang="pt-BR">
-
-<head> -> filho 1
-    <meta charset="UTF-8"> -> filho 2
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> -> filho 2
-    <link rel="stylesheet" href="./assets/css/style.css"> -> filho 2
-    <title>Modelo</title> -> filho 2
-</head>
-
-<body> -> filho 1
-
-    <section class="container"> -> filho 2
-        <h1>Aqui está um modelo. Utilize.</h1> -> filho 3
-        <!-- AQUI --> -> filho 3
-    </section>
-
-    <script src="./assets/js/main.js"></script> -> filho 2
-</body>
-
-</html>
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
 */
 
-// Essa representação nos permite alterar o estilo e a configuração de determinado elemento através do JS, mesmo que essa não seja uma línguagem de JS. Ou seja, pode ser usada em outros tipo de linguagem, também. 
 
-/*
-;)
-*/
+
+// -> Para Objetos:
+const pessoa = {
+    nome: 'Lipas',
+    sobrenome: 'Alfredo',
+    idade: 7,
+    cidade: 'UmLugarAê'
+}
+
+for (let i in pessoa) {
+    console.log(i, pessoa[i]);
+}
--- JS/aula37/index.js
+++ JS/aula37/index.js
@@ -1,21 +1,4 @@
-/* // -> Para Arrays:
-//                0        1      2        3         4         5        6        
-const frutas = ['Pera', 'Maça', 'Uva', 'Banana', 'Laranja', 'Manga', 'Limão'];
-
-// for in -> lê os índices ou chaves do objeto
-for (let i in frutas) {
-    console.log(i);
-    console.log(frutas[i]);
-}
-
-// for (let i = 0; i < frutas.length; i++) {
-//     console.log(frutas[i]);
-// }
-*/
-
-
-
-// -> Para Objetos:
+const nome1 = ['Lipas', 'Alfredo', 'Bartolomeu', '####'];
 const pessoa = {
     nome: 'Lipas',
     sobrenome: 'Alfredo',
@@ -23,6 +6,32 @@ const pessoa = {
     cidade: 'UmLugarAê'
 }
 
-for (let i in pessoa) {
-    console.log(i, pessoa[i]);
+
+/* // -> Objetos 
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
 }
+*/
+
+/* // -> Arrays
+for (let i = 0; i < nome1.length; i++) {
+    console.log(nome1[i]);
+}
+
+for (i in nome1) {
+    console.log(nome1[i]);
+}
+
+for (let valor of nome1) {
+    console.log(valor);
+}
+
+nome1.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+});
+*/
+
+
+// For clássico - Geralmente com iteráveis (arrays ou strings)
+// For In - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si das iteráveis (arrays ou strings)
