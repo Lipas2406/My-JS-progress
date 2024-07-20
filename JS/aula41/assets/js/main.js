7e45f9dde3f6557d386add8f15bcdaf0a335f7e8
Lipas2406 <filipe11severo06@gmail.com>
2024-07-20 11:05:18 -0300

    program logic exercise 2 class

--- JS/aula41/assets/js/main.js
+++ JS/aula41/assets/js/main.js
@@ -1,6 +1,11 @@
+// *Exercício 1
+
 // Escreva uma função que recebe dois números e retorne o maior deles
 
+
 const container = document.querySelector('.container');
+const p = document.createElement('p');
+const h2 = document.createElement('h2');
 const div = document.createElement('div');
 
 
@@ -27,7 +32,54 @@ function randNum() {
 const { maior } = randNum();
 
 container.appendChild(div);
-div.innerHTML = `Número aleatório: ${maior}`
+div.appendChild(h2);
+h2.innerHTML = 'Exercício 1 - Maior dos números:'
+div.appendChild(p);
+p.innerHTML = `Maior número aleatório: ${maior} <br>`
+
 
 
 random();
+
+
+
+
+// *Exercício 2
+
+// Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem (number.
+//Retorne true se a imagem estiver no modo horizontal.
+
+const div2 = document.createElement('div');
+const p2 = document.createElement('p');
+const h22 = document.createElement('h2');
+
+function random(x, y) {
+    const r = Math.random() * (x - y) + y;
+    return Math.floor(r);
+}
+
+function randNum1() {
+    const num1 = random(10, 50);
+    const num2 = random(10, 50);
+
+    return {
+        x: num1,
+        y: num2
+    }
+}
+
+function ePaisagem () {
+    const { x, y } = randNum1();
+    
+    const verde = x > y ? 'Sim' : 'Não'
+
+    container.appendChild(div2);
+    div2.appendChild(h22);
+    h22.innerHTML = 'Exercício 2 - Modo paisagem:'
+    div2.appendChild(p2);
+    p2.innerHTML = `Largura: ${x}px <br> Altura: ${y}px <br> Imagem em modo paisagem: ${verde}`;
+}
+
+ePaisagem();
+
+
