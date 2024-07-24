c890da4cbe162b8f40777f8a693aff6c25c0f32a
Lipas2406 <filipe11severo06@gmail.com>
2024-07-23 22:03:33 -0300

    setInterval and setTimeout class

---
+++ JS/aula44/index.js
@@ -0,0 +1,23 @@
+function mostraHora () {
+    let data = new Date();
+    
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+
+
+const timer = setInterval(function (){
+    console.log(mostraHora());
+}, 1000);
+
+setTimeout(function () {
+    clearInterval(timer);
+}, 6000);
+
+setTimeout(function() {
+    console.log('Não quero mais.')
+}, 7000);
