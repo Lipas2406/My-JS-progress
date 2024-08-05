3619ec409eba4265604cbaf2d186c67ab7382ee9
Lipas2406 <filipe11severo06@gmail.com>
2024-08-05 16:07:42 -0300

    creating a calculator with factory function class

---
+++ JS/aula55/assets/js/main.js
@@ -0,0 +1,82 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+
+        inicia() {
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+
+                if (e.keyCode === 8) {
+                    this.clearOne();
+                }
+            })
+        },
+
+        clearOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if (!conta) {
+                    alert('Conta inválida.');
+                    return;
+                }
+
+            this.display.value = String(conta);
+            } catch (e) {
+                alert('Conta inválida.');
+                return;
+            }
+        },
+
+
+        cliqueBotoes() {
+            // this-> calculadora
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.clearOne();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+
+
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
