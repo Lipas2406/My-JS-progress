e0ef2451b54c48060d2cfc290f5cf67ff260e526
Lipas2406 <filipe11severo06@gmail.com>
2024-12-06 23:27:16 -0300

    basic complex arrays class

--- JS/aula60/index.js
+++ JS/aula60/index.js
@@ -6,8 +6,12 @@ nomes.push('Lipas');
 nomes.unshift('Alfredo');
 
 const novo1 = [...nomes];
-const novo2 = nomes.slice(1, -1);
+const novo2 = nomes.slice(1, -2);
 
 // novo.pop(); 
 delete nomes [2];
-console.log(nomes, novo2); 
+
+const removido = nomes.shift();
+
+console.log(nomes);  
+console.log(novo2);  
