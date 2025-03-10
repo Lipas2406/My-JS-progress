0085ed71f014f081cfecaa85b2fedb4e9076336a
Lipas2406 <filipe11severo06@gmail.com>
2025-03-10 09:45:36 -0300

    ES¨Modules - import/export class

---
+++ JS/aula94/src/index.js
@@ -0,0 +1,5 @@
+// import * as MeuModulo from './modulo1';
+import multiplica, { nome, sobrenome, idade, soma } from './modulo1';
+
+console.log(multiplica(5, 40));
+console.log(nome, sobrenome, idade, soma(10, 10));
---
+++ JS/aula94/src/modulo1.js
@@ -0,0 +1,11 @@
+const nome =  'Lipas';
+const sobrenome = 'Alfredo';
+const idade= 18;
+
+function soma(x, y) {
+    return x + y;
+}
+
+export default (x, y) => x * y;
+
+export { nome, sobrenome, idade, soma};
