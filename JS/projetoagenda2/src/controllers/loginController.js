c427503d67b656f35574ea6fe544e0cc4cf96806
Filipe Gabriel <filipebispo1106@gmail.com>
2026-08-17 22:29:42 -0300

    logging in user, class part 1

--- JS/projetoagenda2/src/controllers/loginController.js
+++ JS/projetoagenda2/src/controllers/loginController.js
@@ -22,3 +22,24 @@ exports.register = async (req, res) => {
     return res.render('404');
   }
 };
+
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(() => res.redirect('/login/index'));
+      return;
+    }
+
+    req.flash('success', 'Login realizado com sucesso!');
+    req.session.user = login.user;
+    req.session.save(() => {return res.redirect('/login/index')});
+  } catch (e) {
+    console.error(e);
+    return res.render('404');
+  }
+};
